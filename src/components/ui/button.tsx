13549837b09ae1b87f26d73bb167131ca54f14a3
Marcos Melo <marcosvnmelo.dev@gmail.com>
2026-07-24 21:12:43 -0400

    chore: format files

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+// oxlint-disable react/only-export-components
 import { Button as ButtonPrimitive } from '@base-ui/react/button';
 import { cva, type VariantProps } from 'class-variance-authority';
 

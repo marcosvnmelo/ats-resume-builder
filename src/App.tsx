744593f33d1cf8dd6b11b170d482417bd6f3af27
Marcos Melo <marcosvnmelo.dev@gmail.com>
2026-07-27 16:38:45 -0400

    feat: sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { TooltipProvider } from '@/components/ui/tooltip';
 import { Builder } from '@/modules/builder/components/builder';
 
 function App() {
   return (
-    <>
+    <TooltipProvider>
       <Builder />
-    </>
+    </TooltipProvider>
   );
 }
 

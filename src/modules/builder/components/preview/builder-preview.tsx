8feb099d93c1e789a6dd94f94f651faf52067d09
Marcos Melo <marcosvnmelo.dev@gmail.com>
2026-07-27 17:19:12 -0400

    refactor: break classes

--- src/modules/builder/components/preview/builder-preview.tsx
+++ src/modules/builder/components/preview/builder-preview.tsx
@@ -1,10 +1,19 @@
+import { cn } from '@/lib/utils';
+
 import { Header } from './header/header';
 import { LeftSide } from './left-side/left-side';
 import { RightSide } from './right-side/right-side';
 
 export function BuilderPreview() {
   return (
-    <div className="light bg-background text-foreground max-w-3xl flex-1 overflow-y-visible p-6 md:mx-auto md:h-screen md:overflow-y-scroll print:p-0">
+    <div
+      className={cn(
+        'light bg-background text-foreground',
+        'p-6 print:p-0',
+        'overflow-y-visible md:overflow-y-scroll',
+        'max-w-3xl flex-1 md:mx-auto md:h-screen',
+      )}
+    >
       <Header />
       <hr className="my-2 border-dashed" />
       <div className="grid grid-cols-3 gap-6">

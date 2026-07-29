0f4f48f2ade1901b6c48984fd78a6b07dcd01da2
Marcos Melo <marcosvnmelo.dev@gmail.com>
2026-07-29 00:40:30 -0400

    feat: intlayer init setup

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,19 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { intlayer } from 'vite-intlayer';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [devtools(), react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  plugins: [
+    devtools(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+    intlayer(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

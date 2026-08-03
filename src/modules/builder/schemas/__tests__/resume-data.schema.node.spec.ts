ad0bc5cd7a89ade515ec78f2887e6c9484bfeaa9
Marcos Melo <marcosvnmelo.dev@gmail.com>
2026-08-03 11:19:30 -0400

    tests: move test case to resume-data.schema.node.spec.ts

--- src/modules/builder/schemas/__tests__/resume-data.schema.node.spec.ts
+++ src/modules/builder/schemas/__tests__/resume-data.schema.node.spec.ts
@@ -1,8 +1,9 @@
 import { describe, expect, it } from 'vitest';
 
 import v0ResumeData from '#tests/constants/json-resumes/v0-resume.json';
+import v1ResumeData from '#tests/constants/json-resumes/v1-resume.json';
 
-import { resumeDataSchema } from '../resume-data.schema';
+import { resumeDataSchema, resumeExportDataSchema } from '../resume-data.schema';
 
 describe('resumeDataSchema', () => {
   it('should parse a v0 resume', () => {
@@ -10,5 +11,18 @@ describe('resumeDataSchema', () => {
 
     expect(parsedData.success).toBe(true);
     expect(parsedData.data).toMatchSnapshot();
+  });
+});
+
+describe('resumeExportDataSchema', () => {
+  it('should add keyAchievementsList extra property to workExperience items', async () => {
+    const resumeData = resumeDataSchema.parse(v0ResumeData);
+    const resumeExportData = resumeExportDataSchema.parse(v0ResumeData);
+
+    expect(resumeData).not.toMatchObject(v1ResumeData);
+    expect(resumeExportData).toMatchObject(v1ResumeData);
+
+    expect(resumeExportData.workExperience.items.length).toBeGreaterThan(0);
+    expect(resumeExportData.workExperience.items.at(0)).toHaveProperty('keyAchievementsList');
   });
 });
